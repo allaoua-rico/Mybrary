0491a3fda69b4b99c3ab8e701ce25f1231ed0e73
allaoua-rico <allaoua.boudriou@gmail.com>
2021-11-20 23:42:30 +0100

    initial author routes

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser= require('body-parser')
 
 const indexRouter = require("./routes/index.js");
+const authorsRouter = require ("./routes/authors.js");
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts)
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}));
+
 app.use('/', indexRouter);
+app.use('/authors',authorsRouter);
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
